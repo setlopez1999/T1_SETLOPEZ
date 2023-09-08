f7ded719a46d8202a7383563c9ee8f93b446f5b4
setlopez1999 <142623394+setlopez1999@users.noreply.github.com>
2023-09-08 14:05:44 -0500

    Add files via upload

---
+++ src/t1/set_lopez/Personas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1.set_lopez;
+
+/**
+ *
+ * @author user
+ */
+public class Personas {
+    
+    //Atributos
+    private String nombre;
+    
+    //Metodos Getter y Setter
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //Constructor
+    public Personas(String nombre){
+        this.nombre = nombre;
+    }
+}
---
+++ src/t1/set_lopez/T1SET_LOPEZ.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1.set_lopez;
+
+/**
+ *
+ * @author user
+ */
+public class T1SET_LOPEZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Inicializamos la listapersona
+        LISTA ventana = new LISTA();
+        
+        //la hacemos visible
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
